cbc9a932b4ce405c07172a4fb21748b8a31d775b
Prakhar Sahu <prakhars762@gmail.com>
2025-04-04 22:14:09 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="mt-40">
+      <Button variant="destructive">Prakhar Sahu</Button>
+    </div>
+  );
+  return <Button variant="destructive">Prakhar Sahu</Button>;
+}
---
+++ components/header.jsx
@@ -0,0 +1,54 @@
+import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { LayoutDashboard } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/image.png"}
+            alt="Viducate Logo"
+            height={60}
+            width={200}
+            className="h-20 w-auto object-contain cursor-pointer"
+          />
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link
+              href={"/dashboard"}
+              className="text-gray-600 hover:text-blue-600 flex items-center gap-2"
+            >
+              <Button variant="outline">
+                <LayoutDashboard size={18} />
+                <span className="hidden md:inline">Dashboard</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "h-10 w-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default Header;
